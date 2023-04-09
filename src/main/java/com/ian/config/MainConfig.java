28ead28f89a0d2a01498939725a0116b6311895e
Ian <iw5420@gmail.com>
2023-04-10 06:09:49 +0800

    @ComponentScan includeFilters使用要加useDefaultFilters = false

--- src/main/java/com/ian/config/MainConfig.java
+++ src/main/java/com/ian/config/MainConfig.java
@@ -10,8 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 
 @Configuration
-@ComponentScan(value="com.ian", excludeFilters = {
-        @ComponentScan.Filter(type=FilterType.ANNOTATION, classes = {Controller.class})})
+@ComponentScan(value="com.ian", includeFilters = {
+        @ComponentScan.Filter(type=FilterType.ANNOTATION, classes = {Controller.class})},
+        useDefaultFilters = false)
 //@ComponentScan value:指定要掃描的包
 //excludeFilters = Filter[], 指定掃描的時候按照什麼規則排除那些組件
 //includeFilters = Filter[], 指定掃描的時候只需要包含哪些組件
