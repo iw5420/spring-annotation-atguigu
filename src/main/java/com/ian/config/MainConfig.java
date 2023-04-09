fdf113cd3dfa79c8d0f24003d267171ae33b9aea
Ian <iw5420@gmail.com>
2023-04-10 06:13:48 +0800

    @ComponentScans 裡面可以多個component scan規則

--- src/main/java/com/ian/config/MainConfig.java
+++ src/main/java/com/ian/config/MainConfig.java
@@ -1,18 +1,20 @@
 package com.ian.config;
 
 import com.ian.bean.Person;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.Configuration.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 
 @Configuration
-@ComponentScan(value="com.ian", includeFilters = {
-        @ComponentScan.Filter(type=FilterType.ANNOTATION, classes = {Controller.class})},
-        useDefaultFilters = false)
+//@ComponentScan(value="com.ian", includeFilters = {
+//        @ComponentScan.Filter(type=FilterType.ANNOTATION, classes = {Controller.class})},
+//        useDefaultFilters = false)
+@ComponentScans(value={
+        @ComponentScan(value="com.ian", includeFilters = {
+                @ComponentScan.Filter(type=FilterType.ANNOTATION, classes = {Controller.class})},
+                useDefaultFilters = false)
+})
 //@ComponentScan value:指定要掃描的包
 //excludeFilters = Filter[], 指定掃描的時候按照什麼規則排除那些組件
 //includeFilters = Filter[], 指定掃描的時候只需要包含哪些組件
